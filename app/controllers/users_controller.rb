ca5b2ae9589f66d3a9c98b8b19e9f45f6241f507
panentka <dai_f62000@yahoo.co.jp>
2015-11-20 09:31:26 +0000

    profile edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
-  
+  before_action :set_user, only:  [:show, :edit, :update, :destroy]
   def show # 追加
    @user = User.find(params[:id])
   end
+  
   def new
     @user = User.new
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -15,11 +17,28 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-
+  
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'ユーザー情報を編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+    
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
+    params.require(:user).permit(:name, :email, :profile, :location, :password,
                                  :password_confirmation)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
   end
 end
